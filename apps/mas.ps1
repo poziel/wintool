bfd5fa289c1f0d158b3f1f2db1fb0cbec926f0af
Poziel <xpoziel@gmail.com>
2025-10-27 12:14:06 -0400

    deleted the envyupdate (since it doesn't exist anymore) and changed the path of the mas.

--- apps/mas.ps1
+++ apps/mas.ps1
@@ -26,4 +26,4 @@
 # To create a shortcut for Microsoft Activation Scripts by MASSGRAVE, execute the following line in PowerShell:
 # $shortcut = (New-Object -ComObject WScript.Shell).CreateShortcut((Join-Path -Path (Get-Location) -ChildPath "MASSGRAVE - Microsoft Activation Scripts.lnk")); $shortcut.TargetPath = "%windir%\System32\WindowsPowerShell\v1.0\powershell.exe"; $shortcut.Arguments = "-ExecutionPolicy Bypass -Command `"Start-Process powershell.exe -verb runas -ArgumentList 'irm https://raw.githubusercontent.com/poziel/wintool/refs/heads/master/apps/mas.ps1 | iex'`""; $shortcut.WorkingDirectory = "%windir%\System32\WindowsPowerShell\v1.0"; $shortcut.Description = "Microsoft Activation Scripts (MAS) by MASSGRAVE is a script suite for activating Windows and Office products with flexible options."; $shortcut.Save()
 
-irm https://massgrave.dev/get | iex
+irm https://get.activated.win | iex
